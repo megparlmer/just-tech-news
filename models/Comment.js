97b97a6c70d7673a773162be21b071a4b02df0c9
megparlmer <megparlmer@gmail.com>
2022-05-05 09:26:57 -0400

    created homepage

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const { post } = require('../routes');
+const { post } = require('../controllers');
 
 class Comment extends Model {}
 
