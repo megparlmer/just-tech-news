abbbd5bd9d9244abf7b4e6df47c30f6e8e35027c
megparlmer <megparlmer@gmail.com>
2022-05-06 13:22:06 -0400

    login and logout function

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,11 +63,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
 });
 
 //POST
@@ -90,8 +94,17 @@ router.post('/login', (req, res) => {
             return;
         }
 
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
         res.json({ user: dbUserData, message: 'You are now logged in' });
+        });
+    
     });
+
 });
 
 //PUT /api/users/1
@@ -140,6 +153,16 @@ router.delete('/:id', (req, res) => {
                 res.status(500).json(err);
             });
     });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
